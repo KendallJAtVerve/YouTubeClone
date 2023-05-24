6875cdfab901b076c09251947486c7cd54c4afb0
KendallJAtVerve <97098881+KendallJAtVerve@users.noreply.github.com>
2023-05-24 07:56:37 +0100

    Inital installation and setup of YouTube clone

---
+++ youtube-clone/resources/js/Pages/Welcome.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="YouTube" />
+    Cool
+</template>
