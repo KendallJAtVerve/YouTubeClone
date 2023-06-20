6b4bae8f79a10f3a56ebd98560e311021bbd65d5
KendallJAtVerve <97098881+KendallJAtVerve@users.noreply.github.com>
2023-06-20 14:30:54 +0100

    Side navigation setup and sidenavitem component created

---
+++ youtube-clone/resources/js/Components/SideNavItem.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { defineProps, toRefs } from 'vue';
+import Home from 'vue-material-design-icons/Home.vue';
+import FolderPlayOutline from 'vue-material-design-icons/FolderPlayOutline.vue';
+import ImageMultiple from 'vue-material-design-icons/ImageMultiple.vue';
+import ThumbUpOutline from 'vue-material-design-icons/ThumbUpOutline.vue';
+import History from 'vue-material-design-icons/History.vue';
+import ClockOutline from 'vue-material-design-icons/ClockOutline.vue';
+import VideoPlusOutline from 'vue-material-design-icons/VideoPlusOutline.vue';
+import DeleteIcon from 'vue-material-design-icons/Delete.vue';
+
+const props = defineProps({
+    openSideNav: Boolean,
+    iconString: String, 
+});
+
+const { openSideNav, iconString } = toRefs(props);
+
+let icon = null;
+
+if (iconString.value === 'Home') icon = Home;
+if (iconString.value === 'Subscriptions') icon = FolderPlayOutline;
+if (iconString.value === 'Library') icon = ImageMultiple;
+if (iconString.value === 'Liked') icon = ThumbUpOutline;
+if (iconString.value === 'History') icon = History;
+if (iconString.value === 'Watch Later') icon = ClockOutline;
+if (iconString.value === 'Add Video') icon = VideoPlusOutline;
+if (iconString.value === 'Delete Video') icon = DeleteIcon;
+</script>
+<template>
+    <li 
+        :class="[!openSideNav ? 'p-2' : 'flex items-center p-2']"
+        class="text-sm font-semibold text-center text-white rounded-lg cursor-pointer hover:bg-gray-700">
+        <div :class="[!openSideNav ? 'w-full flex justify-center' : '']">
+            <component :is="icon" />
+        </div>
+        <div :class="[!openSideNav ? '' : 'mt-1 ml-4']">
+            <span v-if="!openSideNav">{{ iconString.substring(0, 4) }}</span>
+            <span v-else>{{ iconString }}</span>
+        </div>
+    </li>
+</template>
--- youtube-clone/resources/js/Layouts/NavLayout.vue
+++ youtube-clone/resources/js/Layouts/NavLayout.vue
@@ -3,18 +3,24 @@ import { ref } from 'vue';
 import { Link } from '@inertiajs/vue3';
 import MenuIcon from 'vue-material-design-icons/Menu.vue';
 import MagnifyIcon from 'vue-material-design-icons/Magnify.vue';
+import SideNavItem from '@/Components/SideNavItem.vue';
 
+// NOTE TO SELF
+// Run npm run dev
+// Run php artisan serve
+
+let openSideNav = ref(true);
 </script>
 
 <template>
     <div class="relative">
         <div id="TopNav" class="w-[100%] h-[60px] fixed bg-black z-20 flex items-center justify-between">
             <div class="flex items-center">
-                <button class="p-2 ml-3 rounded-full hover:bg-gray-800 inline-block cursor-pointer">
+                <button @click="openSideNav = !openSideNav" class="inline-block p-2 ml-3 rounded-full cursor-pointer hover:bg-gray-800">
                     <MenuIcon fillColor="#FFFFFF" :size="26" />
                 </button>
                 <div class="mx-2"></div>
-                <div class="flex text-white items-center justify-center mr-10 cursor-pointer">
+                <div class="flex items-center justify-center mr-10 text-white cursor-pointer">
                     YOUTUBE
                 </div>
             </div>
@@ -53,14 +59,44 @@ import MagnifyIcon from 'vue-material-design-icons/Magnify.vue';
 
             <div>
                 <img
-                    class="rounded-full mx-8"
+                    class="mx-8 rounded-full"
                     width="35"
                     src="https://yt3.ggpht.com/yti/AHyvSCAFJ6g9MbHJKTba65j-9N4ZPPkODN5_VnFGkez5=s88-c-k-c0x00ffffff-no-rj-mo"
                 />
             </div>
         </div>
-        <div id="SideNav">
-        
+        <div
+            id="SideNav"
+            class="h-[100%] fixed z-0 bg-black"
+            :class="[!openSideNav ? 'w-[70px]' : 'w-[240px]']"
+        >
+            <ul 
+                class="mt-[60px] w-full"
+                :class="[!openSideNav ? 'p-2' : 'px-5 pb-2 pt-[7px]']">
+                <SideNavItem :openSideNav="openSideNav" iconString="Home" />
+                <SideNavItem :openSideNav="openSideNav" iconString="Add Video" />
+                <SideNavItem :openSideNav="openSideNav" iconString="Delete Video" />
+                <div class="border-b border-b-gray-700 my-2.5"></div>
+                <SideNavItem :openSideNav="openSideNav" iconString="Subscriptions" />
+                <SideNavItem :openSideNav="openSideNav" iconString="Library" />
+                <SideNavItem :openSideNav="openSideNav" iconString="Liked" />
+                <SideNavItem :openSideNav="openSideNav" iconString="History" />
+                <SideNavItem :openSideNav="openSideNav" iconString="Watch Later" />
+                <div v-if="openSideNav">
+                    <div class="border-b border-b-gray-700 my-2.5"></div>
+                    <div class="text-gray-400 text-[14px] text-extrabold">
+                        About Press Copyright
+                        <div>Contact us</div>
+                        Creator Advertise Developers
+                    </div>
+                    <div class="border-b border-b-gray-700 my-2.5"></div>
+                    <div class="text-gray-400 text-[14px] text-extrabold">
+                        Terms Privacy Policy & Safety
+                        <div>How YouTube works</div>
+                        <span>Test new features</span>
+                    </div>
+                </div>
+            </ul>
         </div>
         <div id="SideNavOverlay">
         
